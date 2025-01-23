592c1c9bcccfd10c1916724b747bc09602cfc802
hanna-uler <hannauler@gmail.com>
2025-01-23 16:10:49 -0500

    completed task-01

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,28 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-};
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+function isEnoughCapacity(products, conteinerSize) {
+    const itemQuants = Object.values(products);
+    let totalCount = 0
+    for (let i = 0; i < itemQuants.length; i++) {
+        totalCount += itemQuants[i]
+    }
+    if (totalCount <= conteinerSize) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+);
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+);
