17fd5be038b83143f5f1234172a8b4136fb309bd
hanna-uler <hannauler@gmail.com>
2025-01-23 17:09:44 -0500

    start task-02

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,47 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`
-};
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+function calcAverageCalories(days) {
+    let totalCcal = 0;
+    for (let i = 0; i < days.length; i++) {
+        const oneDayCcalCount = days[i].?calories-HowToGetIt?;
+        totalCcal += oneDayCcalCount
+    }
+    return totalCcal / days.length;
+}
+
+// Напиши функцію calcAverageCalories(days), 
+// яка повертає середньодобове значення кількості калорій, 
+// які спортсмен споживав протягом тижня. Функція очікує один параметр: 
+// days — масив обєктів. Кожен обєкт описує день тижня та 
+// кількість калорій calories, спожитих спортсменом, у цей день. 
+// Візьми код нижче і встав після оголошення своєї функції 
+// для перевірки коректності її роботи. У консоль будуть виведені результати її викликів.
+
+
+
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+);
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+);
+
+console.log(
+  calcAverageCalories([])
+);
